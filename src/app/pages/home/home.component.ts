5bfba2b7359d95cefb99de650d0b80ac8b09496f
master762 <m8069248@gmail.com>
2025-04-07 00:25:56 +0300

    начало первой страницы

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { HeroComponent } from '../../components/hero/hero.component';
+import { RatingComponent } from '../../components/rating/rating.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true, 
-  imports: [HeaderComponent, HeroComponent],
+  imports: [HeaderComponent, HeroComponent, RatingComponent],
 })
 export class HomeComponent {}
