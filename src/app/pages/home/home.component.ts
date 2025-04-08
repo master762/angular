27191aee4b75c962d3ee40da236ca055a216af1e
master762 <m8069248@gmail.com>
2025-04-08 23:04:41 +0300

    половина первой страницы

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,12 +3,14 @@ import { HeaderComponent } from '../../components/header/header.component';
 import { HeroComponent } from '../../components/hero/hero.component';
 import { RatingComponent } from '../../components/rating/rating.component';
 import { PackagesComponent } from '../../components/packages/packages.component';
+import { BookComponent } from '../../components/book/book.component';
+import { WhyUsComponent } from '../../components/why-us/why-us.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true, 
-  imports: [HeaderComponent, HeroComponent, RatingComponent,PackagesComponent],
+  imports: [HeaderComponent, HeroComponent, RatingComponent,PackagesComponent, BookComponent, WhyUsComponent],
 })
 export class HomeComponent {}
